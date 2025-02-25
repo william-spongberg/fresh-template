fb4abdff4eb5b29fdbbf41304441a77e276de3f5
William Spongberg <william.spongberg@outlook.com>
2025-02-25 13:46:08 +1100

    Add basic index page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,20 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import * as Layout from '../components/Layout.tsx';
+import * as Text from '../components/Text.tsx';
+import * as Icons from "../components/Icons.tsx";
+import Button from "../components/Button.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
+    <Layout.Page>
+      <Layout.Element>
+        <Text.Title>Welcome to the fresh-template</Text.Title>
+        <Text.Paragraph>
+          This is a fresh-template project.
+        </Text.Paragraph>
+        <Button href="https://github.com/william-spongberg/fresh-template">
+          <Icons.GitHub />
+        </Button>
+      </Layout.Element>
+    </Layout.Page>
   );
 }
