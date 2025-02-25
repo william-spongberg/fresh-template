36a0f0ffe2fbb9e1b7c9b29e6b063dd4add666c4
William Spongberg <william.spongberg@outlook.com>
2025-02-25 13:45:28 +1100

    update 404 and app page for new components

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,4 +1,7 @@
 import { Head } from "$fresh/runtime.ts";
+import Button from "../components/Button.tsx";
+import * as Layout from "../components/Layout.tsx";
+import * as Text from "../components/Text.tsx";
 
 export default function Error404() {
   return (
@@ -6,22 +9,20 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
-          </p>
-          <a href="/" class="underline">Go back home</a>
-        </div>
-      </div>
+      <Layout.Page>
+        <Layout.Element>
+          <Text.Title>404 - Page not found</Text.Title>
+          <br />
+          <Text.Paragraph>
+            The page you were looking for does not exist.
+          </Text.Paragraph>
+          <Button href="/">
+            <Text.SubHeading>
+              Go back home
+            </Text.SubHeading>
+          </Button>
+        </Layout.Element>
+      </Layout.Page>
     </>
   );
 }
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,7 +1,9 @@
 import { type PageProps } from "$fresh/server.ts";
+import * as Layout from "../components/Layout.tsx";
+
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,7 +11,12 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
+        <Layout.Header title="fresh-template" />
         <Component />
+        <Layout.Footer
+          isBeta
+          author="Author"
+        />
       </body>
     </html>
   );
